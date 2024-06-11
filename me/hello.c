89891b1d3064af9529f2d5e3deddda0f157d08d5
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-11 13:41:53 +0000

    Tue, Jun 11, 2024, 7:11 PM +05:30

--- me/hello.c
+++ me/hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 int main(void)
 {
     string name = get_string("what is your name?");
-    printf("hello, %s\n",name);
+    printf("hello, %s\n", name);
 }
