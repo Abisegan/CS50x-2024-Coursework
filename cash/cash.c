221055793b684fca8991dbebc827a44101a287e3
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-12 10:23:57 +0000

    Wed, Jun 12, 2024, 3:53 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -3,5 +3,6 @@
 int main (void)
 {
     // prompt the user for change owed, in cent
-    //
+    // calculate how many quaters you should give customer
+    // subtract the value of the quaters from cent
 }
