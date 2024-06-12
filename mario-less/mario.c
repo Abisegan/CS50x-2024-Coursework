18aaa659a89ed86711115dd75ffec515d482db97
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-12 03:07:37 +0000

    Wed, Jun 12, 2024, 8:37 AM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,2 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+int main(void)
+{
+    // prompt the user for the pyrmid's hight
+    int n;
+    do
+    {
+        n = get_int("Hight: ");
+    }
+    while(n < 0 || n <)
+
+
+
+    // print the pyrmid of the hight
+}
