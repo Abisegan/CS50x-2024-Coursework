3d62975047b315fd875e8b242028905e700fba27
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-13 05:28:26 +0000

    Thu, Jun 13, 2024, 10:58 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -39,9 +39,9 @@ int validity_check(int number,int digits)
 {
     int checksum = 0;
     int x = 100;
-    for (int i= 0;i = digits / 2; i++)
+    for (int i= 0;i == digits / 2; i++)
     {
-        checksum = checksum + number%x * 2
+        checksum = checksum + number%x * 2;
         x = x * 100;
     }
     return checksum;
