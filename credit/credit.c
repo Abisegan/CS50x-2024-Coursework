0aef081fd4759984243d2c78808f4dfc411f8b74
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-14 00:36:24 +0000

    Fri, Jun 14, 2024, 6:06 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -14,7 +14,7 @@ int main(void)
     int last= checksum(number);
     int digit = digit_count(number);
     int s_digit = start_digit(number,digit);
-    printf("%i\n",s_digit);
+    printf("%i\n",digit);
     if (last % 10 == 0)
     {
         printf("valid: \n");
