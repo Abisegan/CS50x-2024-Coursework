a602d9ae6d706b9428d3a05a12322b8964dceb19
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-17 10:59:45 +0000

    Mon, Jun 17, 2024, 4:29 PM +05:30

---
+++ scrabble/scrabble.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// points assigned to each letter of the alphabet
+int points[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+int compute_score(string word);
+int main(void)
+{
+    // prompt the user for two words
+    string word1 = get_string("player: ");
+    string word2 = get_string("player: ");
+
+    // compute the score of each word
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+    // print the winner
+}
+int compute_score(string word)
+{
+    //keep track of score
+    int score = 0;
+    // compute and return score for word
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score = 
+        }
+    }
+}
