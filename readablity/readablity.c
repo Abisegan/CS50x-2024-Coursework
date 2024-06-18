1a3831453dd7a23a5ff41a23d8c5de5245f80e6b
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-18 03:21:33 +0000

    Tue, Jun 18, 2024, 8:51 AM +05:30

---
+++ readablity/readablity.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+
+
+
+
+int main(void)
+{
+    // prompt the user for some text
+    string text = 
+    // count the number of letters, words and the sentences in the text
+    // compute the colemen-liau index
+    // print the grade level
+
+
+}
