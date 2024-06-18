bb939315fae468fac4f049e317a01b4dcf80be77
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-18 03:24:11 +0000

    Tue, Jun 18, 2024, 8:54 AM +05:30

--- readablity/readablity.c
+++ readablity/readablity.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <stdio.h>
 
 
 
@@ -6,7 +7,7 @@
 int main(void)
 {
     // prompt the user for some text
-    string text = 
+    string text = get_string("Text: ");
     // count the number of letters, words and the sentences in the text
     // compute the colemen-liau index
     // print the grade level
