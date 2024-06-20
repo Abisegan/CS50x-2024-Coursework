26e77267a8ae0cce01262c0fb0cc559619879559
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-20 04:30:45 +0000

    Thu, Jun 20, 2024, 10:00 AM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+bool only_digit(string key)
 int main(int argc,string argv[])
 {
     // get key
     string key = argv[1];
-    if (argc == 2)
+    bool only_digit = only_digit( key);
+    if ((argc == 2) && (only_digit == true))
     {
 
         printf("%s",key);
+        return 0;
     }
     else
     {
