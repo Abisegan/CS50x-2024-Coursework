e5a1944b93226945758c67ee87bba8966dcb20bd
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-20 15:25:20 +0000

    Thu, Jun 20, 2024, 8:55 PM +05:30

--- caesar/test.c
+++ caesar/test.c
@@ -5,5 +5,22 @@
 #include <stdlib.h>
 int main(void)
 {
-    
+    char c;
+    char p = 'a';
+    if (isalpha(p))
+    {
+        if (isupper(p))
+        {
+            c = ((p - 'A') + n) % 26;
+        }
+        else
+        {
+            c = ((p - 'a') + n) % 26;
+        }
+    }
+    else
+    {
+        c = p;
+    }
+    printf("%c",c);
 }
