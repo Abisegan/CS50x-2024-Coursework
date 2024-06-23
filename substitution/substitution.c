2c5c1cc7b1184d00360e53dc2779e60ed95b615c
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-23 21:42:41 +0000

    Mon, Jun 24, 2024, 3:12 AM +05:30

---
+++ substitution/substitution.c
@@ -0,0 +1,20 @@
+# include <cs50.h>
+# include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // get key
+    // validate key
+    if (argc == 25)
+    {
+        if ()
+    }
+    else
+    {
+        printf("key must contain 26 characters");
+        return 1;
+    }
+    // get plain text
+    // enciper
+    // print cipher text
+}
