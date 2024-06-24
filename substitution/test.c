ff14237f3118744517ff8d64bf940d5266af3048
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-24 17:14:37 +0000

    Mon, Jun 24, 2024, 10:44 PM +05:30

---
+++ substitution/test.c
@@ -0,0 +1,18 @@
+# include <cs50.h>
+# include <stdio.h>
+# include <ctype.h>
+# include <string.h>
+
+int main(int argc,string argv[])
+{
+
+
+    string key[1];
+    for (int x = 0; x < 25; x++)
+    {
+        key[0][x] = tolower(key1[x]);
+
+    }
+    printf("%s",key[0]);
+
+}
