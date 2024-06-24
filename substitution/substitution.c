78bf90d483590bf4e634bf3dad701f4fdcde812e
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-24 15:44:34 +0000

    Mon, Jun 24, 2024, 9:14 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,8 +3,8 @@
 # include <ctype.h>
 # include <string.h>
 char alphabatic[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-bool alpha_check(string key2[]);
-bool repeat_check(string key2[]);
+bool alpha_check(string key2);
+bool repeat_check(string key2);
 string to_lower(string key1);
 
 int main(int argc, string argv[])
