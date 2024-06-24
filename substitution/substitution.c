cae2072f29d7cb9fb53a4cb314a8c0b08248ed5e
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-24 12:23:02 +0000

    Mon, Jun 24, 2024, 5:53 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -79,10 +79,10 @@ bool alpha_check(string key2)
 }
 string to_lower(string key1)
 {
-    string key2;
+   
     for (int x = 0; x < 26; x++)
     {
-        key2[x] = tolower(key1[x]);
+        string key2[x] = tolower(key1[x]);
 
     }
     return key2;
