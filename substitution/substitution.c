1d5375d0c85106373fa1e1cdc56287298eef403f
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-25 18:01:07 +0000

    Tue, Jun 25, 2024, 11:31 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -22,7 +22,7 @@ int main(int argc, string argv[])
 
     }
 
-    string key2 = key;
+    string key2 = key[0];
     bool alpha = alpha_check(key2);
     bool repeat = repeat_check(key2);
     // validate key
