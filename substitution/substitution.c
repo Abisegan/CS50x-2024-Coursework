da06c3efea65166f193a192a3d8a316611d57090
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-25 17:55:11 +0000

    Tue, Jun 25, 2024, 11:25 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -16,8 +16,8 @@ int main(int argc, string argv[])
     string key[1];
     for (int x = 0; x < 26; x++)
     {
-        key[0][25];
-        key[0][x] = tolower(key1[x]);
+        key3[25] = key[0];
+        key3[x] = tolower(key1[x]);
 
 
     }
