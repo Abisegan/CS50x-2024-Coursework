2ca3d35d27d63475f37b18858cfaa2fbe3f4c11b
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-26 06:04:02 +0000

    Wed, Jun 26, 2024, 11:34 AM +05:30

--- substitution/test.c
+++ substitution/test.c
@@ -6,7 +6,15 @@
 int main(int argc,string argv[])
 {
     string key2 = argv[1];
-   
+   string plaintext = get_string("plaintext: ");
+    // enciper
+    string ciphertext[1];
+    for(int i = 0,len = strlen(plaintext); i < len; i++)
+    {
+        ciphertext[0][i] = key2[plaintext[i] - 'a'];
+    }
+    printf("%s",ciphertext[0]);
+    // print cipher text
     printf("%i",n);
 
 }
