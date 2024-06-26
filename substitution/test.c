7b7b1008a2bf14450f411143a8de3b62a4a0426a
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-06-26 06:03:24 +0000

    Wed, Jun 26, 2024, 11:33 AM +05:30

--- substitution/test.c
+++ substitution/test.c
@@ -6,21 +6,7 @@
 int main(int argc,string argv[])
 {
     string key2 = argv[1];
-    int n = 0;
-    for (int i = 0; i < 26; i++)
-    {
-        if (isalpha(key2[i]))
-        {
-            n = 0;
-            printf("%c",key2[i]);
-        }
-        else
-        {
-            n = 1;
-            i = 27;
-        }
-
-    }
+   
     printf("%i",n);
 
 }
