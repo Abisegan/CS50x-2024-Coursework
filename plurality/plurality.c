3c05925351dfab3951b4b586a8dcd06cc19fe1c0
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-04 13:11:57 +0000

    Thu, Jul 4, 2024, 6:41 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,9 +71,8 @@ bool vote(string name)
         if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
-            
+            return true;
         }
-        return true;
         else
         {
             return false;
