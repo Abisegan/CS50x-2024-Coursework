3dc5fc9dd0dcf54696cd339a9ac9dd0ace424f79
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-04 17:13:10 +0000

    Thu, Jul 4, 2024, 10:43 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,7 +68,7 @@ bool vote(string name)
     // TODO
     for (int i = 0; i <= MAX; i++)
     {
-        if (strlen(candidates[i].name)<1)
+        if (strlen(candidates[i].name) == NULL)
         {
             return false;
         }
