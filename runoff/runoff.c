74f264c1ff2a49e3d26727d569f95eed08bbd40a
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-05 10:12:39 +0000

    Fri, Jul 5, 2024, 3:42 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,10 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    for (int i = 0; i < ;i++)
+    for (int i = 0; i < candidate_count;i++)
+    {
+        if (strcmp(,name) == 0)
+    }
     return false;
 }
 
