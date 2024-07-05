f259d7949a3419003f63271ed8ec9a64935a6f37
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-05 04:12:57 +0000

    Fri, Jul 5, 2024, 9:42 AM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,14 +66,13 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for ( int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name, name) == 0 )
+        if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
             return true;
         }
-
     }
     return false;
 }
@@ -94,7 +93,7 @@ void print_winner(void)
     {
         if (Mvotes == candidates[j].votes)
         {
-            printf("%s\n",candidates[j].name);
+            printf("%s\n", candidates[j].name);
         }
     }
     return;
