a1a81da9b20040ed83dbf007b43abe6b307e0f8b
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-06 14:09:38 +0000

    Sat, Jul 6, 2024, 7:39 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -149,9 +149,10 @@ void tabulate(void)
             if (candidates[preferences[i][j]].eliminated == false)
             {
                 candidates[preferences[i][j]].votes++;
-                break;
+
             }
         }
+        break;
     }
     return;
 }
