7e4b694c28077d022be601890628dc08ae46ab54
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-06 12:44:43 +0000

    Sat, Jul 6, 2024, 6:14 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -178,8 +178,9 @@ int find_min(void)
     int min = 0;
     for (int i = 0; i < candidate_count - 1; i++)
     {
-        if (candidates[i].eliminated == false && candidates[i].votes < candidates[i + 1].votes)
+        if (candidates[i].eliminated == false)
         {
+            if (can)
             min = candidates[i].votes;
         }
 
