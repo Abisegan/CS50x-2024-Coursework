9eb8c2b21697a90e456cf3613c5b3acc085b61d9
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-06 12:28:40 +0000

    Sat, Jul 6, 2024, 5:58 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -182,9 +182,9 @@ int find_min(void)
         {
             min = candidates[i].votes;
         }
-        return min;
+
     }
-    return 0;
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
