bbcd9ebd19afc70c8c84f4dc6ad84a85e84dc641
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-06 08:49:56 +0000

    Sat, Jul 6, 2024, 2:19 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,7 +191,7 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    for (int i = 0; i < candidate_count)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
         {
