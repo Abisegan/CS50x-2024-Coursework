92cb3216794f11261ee303cd536a5ed3554060c2
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-17 03:19:28 +0000

    Wed, Jul 17, 2024, 8:49 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -104,13 +104,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 {
                     if (m > 0 && n > 0 && m <= width && n <= height)
                     {
-                        image[k][l].rgbtRed = round((rgbtRed + copy[m][n].rgbtRed)/pix_count)
-                        image[k][l].rgbtGreen = round((rgbtGreen + copy[m][n].rgbtGreen)/pix_count)
-                        image[k][l].rgbtBlue = round((rgbtBlue + copy[m][n].rgbtBlue)/pix_count)
+                        image[k][l].rgbtRed = round((rgbtRed + copy[m][n].rgbtRed)/pix_count);
+                        image[k][l].rgbtGreen = round((rgbtGreen + copy[m][n].rgbtGreen)/pix_count);
+                        image[k][l].rgbtBlue = round((rgbtBlue + copy[m][n].rgbtBlue)/pix_count);
                     }
                 }
             }
         }
     }
-
+    return;
 }
