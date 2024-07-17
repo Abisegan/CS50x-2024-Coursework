5caab09fda2186eec3203b216707a66410ae3a0e
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-17 17:08:05 +0000

    Wed, Jul 17, 2024, 10:38 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int color = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            int color =
+                round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
             image[i][j].rgbtRed = color;
             image[i][j].rgbtGreen = color;
             image[i][j].rgbtBlue = color;
@@ -23,9 +24,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
-            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
-            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen +
+                                 0.189 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen +
+                                   0.168 * image[i][j].rgbtBlue);
+            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen +
+                                  0.131 * image[i][j].rgbtBlue);
 
             if (sepiaRed > 255)
             {
@@ -64,16 +68,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
 
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             temp = image[i][j];
 
-
             image[i][j] = image[i][width - j - 1];
 
-
             image[i][width - j - 1] = temp;
-
         }
     }
     return;
@@ -112,9 +113,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                 }
             }
-            image[k][l].rgbtRed = round((rgbtRed)/pix_count);
-            image[k][l].rgbtGreen = round((rgbtGreen)/pix_count);
-            image[k][l].rgbtBlue = round((rgbtBlue)/pix_count);
+            image[k][l].rgbtRed = round((rgbtRed) / pix_count);
+            image[k][l].rgbtGreen = round((rgbtGreen) / pix_count);
+            image[k][l].rgbtBlue = round((rgbtBlue) / pix_count);
         }
     }
     return;
