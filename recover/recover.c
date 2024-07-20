3c146b0fa001e56ceed2edb0dded4862c657bfcd
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-20 09:09:22 +0000

    Sat, Jul 20, 2024, 2:39 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && 0xe0 < buffer[3] >0xef)
         {
-            fwrite(buffer, 1, 512, card)
+            fwrite(buffer, 1, 512, card);
         }
     }
     // Create jpg from the data
