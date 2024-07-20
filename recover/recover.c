bb0c5bce973a44d26ec9cf2e52885f3222be56cf
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-20 08:44:16 +0000

    Sat, Jul 20, 2024, 2:14 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
     // While there's still data left to read from the memory card
-    while (fread(buffer, 1, 512, card.r))
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        
+    }
     // Create jpg from the data
 
 }
