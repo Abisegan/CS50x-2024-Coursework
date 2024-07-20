723045d49ae529e1add89b47d8ea4f919a15b38f
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-20 08:19:01 +0000

    Sat, Jul 20, 2024, 1:49 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     // Accept a single command-line argument
+    if (argc !== 2)
+    {
+        printf("usage: ./recover FILE\n");
+        retuen 1;
+    }
     // Open the memory card
     // While there's still data left to read from the memory card
     // Create jpg from the data
