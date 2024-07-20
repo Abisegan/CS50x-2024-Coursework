d53524489479df6da0fb320f5951a28e62e2ddc5
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-20 08:22:06 +0000

    Sat, Jul 20, 2024, 1:52 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
         retuen 1;
     }
     // Open the memory card
+    FILE *card = fopen(argv[1], "r");
     // While there's still data left to read from the memory card
     // Create jpg from the data
 
