dbec9eb122d64884bb783023058bb084c661fe83
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-21 04:19:47 +0000

    Sun, Jul 21, 2024, 9:49 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xff) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             FILE *card1 = fopen()
             fwrite(buffer, 1, 512, card1);
