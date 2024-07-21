470395356bb0e764934c479919df72bf7305da07
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-21 05:21:48 +0000

    Sun, Jul 21, 2024, 10:51 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -26,12 +26,12 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             sprintf(name, "%03i.jpg", i);
-            
+            fwrite(buffer, 1, 512, img);
             i++;
         }
         else
         {
-
+            fwrite(buffer, 1, 512, img);
         }
     }
     // Create jpg from the data
