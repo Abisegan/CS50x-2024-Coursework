3dadc5df129e760c173aa7209e0fbd2602de2635
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-07-21 05:05:44 +0000

    Sun, Jul 21, 2024, 10:35 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
     // While there's still data left to read from the memory card
     FILE *img = fopen()
-    char *name = malloc(7sizeof(char))
+    char *name = malloc(8 * sizeof(char));
     while (fread(buffer, 1, 512, card) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
