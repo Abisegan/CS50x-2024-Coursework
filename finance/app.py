01f1cd3f915aecf4946c8caad305db18a65d3965
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-09 12:04:05 +0000

    Sat, Nov 9, 2024, 5:34 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -36,7 +36,7 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     transactions =  db.execute(
-            "SELECT symbol, SUM(shares) AS shares, price FROM users WHERE id = ?", session["user_id"]
+            "SELECT symbol, SUM(shares) AS shares, price FROM transactions WHERE id = ?", session["user_id"]
         )[0]["cash"]
     return apology("TODO")
 
