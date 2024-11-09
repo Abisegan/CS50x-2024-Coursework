7f6ce1d9215d0032e5ae22710830c3d2f82cc220
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-09 02:20:42 +0000

    Sat, Nov 9, 2024, 7:50 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -71,8 +71,8 @@ def buy():
             "UPDATE users SET cash = ? WHERE id = ?", new_cash, session["user_id"]
         )
         db.execute(
-                "INSERT INTO transactions (user_id, symbol, shares, price, transaction_type) VALUES (?, ?, ?, ?, buy)", session["user_id"] , quote_det["symbol"], shares, quote_det["price"]
-                )
+                "INSERT INTO transactions (user_id, symbol, shares, price, transaction_type) VALUES (?, ?, ?, ?, buy)", session["user_id"], quote_det["symbol"], shares, quote_det["price"]
+        )
         return redirect("/")
 
 
