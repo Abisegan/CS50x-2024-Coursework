d7572eeedb563ab0cd135ad9215300cdabebeba8
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-10 06:30:18 +0000

    Sun, Nov 10, 2024, 12:00 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -92,7 +92,7 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    transactions = db.execute()
 
 
 @app.route("/login", methods=["GET", "POST"])
