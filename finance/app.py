151f2ba7495cc93453792f95ccf5986b66c6cd3b
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-10 05:34:48 +0000

    Sun, Nov 10, 2024, 11:04 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -228,5 +228,5 @@ def sell():
 
     else:
         symbols_ava = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol HAVING SUM(shares) > 0", session["user_id"])
-        return render_template("sell.html", symbols_ava = symbols_ava)
+        return render_template("sell.html", symbols = symbols_ava)
 
