2c8f02a2031c88b71ebd16be170c16228bb44e93
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-10 03:52:52 +0000

    Sun, Nov 10, 2024, 9:22 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -201,7 +201,6 @@ def sell():
     if request.method == "GET":
         return render_template("sell.html")
     else:
-          symbols_ava = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol HAVING SUM(shares) > 0", session["user_id"])
-        for symbol in symbols_ava:
+        symbols_ava = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol HAVING SUM(shares) > 0", session["user_id"])
         return render_template("sell.html", symbols = symbols_ava)
 
