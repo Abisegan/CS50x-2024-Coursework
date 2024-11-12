61dea4c9b556990905462e8b630e26287d56ab33
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-12 03:17:22 +0000

    Tue, Nov 12, 2024, 8:47 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -17,7 +17,7 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+# I created transactions table from rubber duck's suggested code
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
