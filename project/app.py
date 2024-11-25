41c30a1e212ae8dd4cc346529606f7cf012b6eaf
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-25 18:45:52 +0000

    Tue, Nov 26, 2024, 12:15 AM +05:30

--- project/app.py
+++ project/app.py
@@ -82,7 +82,7 @@ def signup():
         hash = generate_password_hash(password)
         try:
             insert = db.execute("INSERT INTO users (username, hash, role) VALUES (?, ?, ?)",
-                                  username, hash, 'user')
+                                  username, hash, 'admin')
 
         except:
             return apology("signup.html", "username already exists")
