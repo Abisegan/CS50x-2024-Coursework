47c0536eb927618af6ab657e5d83cfa759497fe8
Abisegan <165914287+Abisegan@users.noreply.github.com>
2024-11-25 18:27:50 +0000

    Mon, Nov 25, 2024, 11:57 PM +05:30

--- project/app.py
+++ project/app.py
@@ -11,7 +11,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///repair_shop.db")
+db = SQL("sqlite:///electro_dr.db")
 
 STATUSES = [
     "We Will Reach You",
